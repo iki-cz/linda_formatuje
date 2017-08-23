f6f628bef7b8e26fe992d9d42c0b444392158b39
iki <iki@iki-pc>
2017-08-23 06:42:00 +0200

    item price corrections

--- src/EventSubscriber/ItemPriceSubscriber.php
+++ src/EventSubscriber/ItemPriceSubscriber.php
@@ -3,23 +3,28 @@ namespace Drupal\linda_formatuje\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\Event;
-use Drupal\commerce_cart\Event;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+// use Drupal\commerce_cart\Event\CartEvents;
+use Drupal\commerce_cart\Event\CartEntityAddEvent;
+use Drupal\commerce_price\Price;
 
 /**
  * Class ItemPriceSubscriber.
  */
 class ItemPriceSubscriber implements EventSubscriberInterface {
 
-  public function updateItemPrice(FilterResponseEvent $event) {
-  	$response = $event->getResponse();
+  public function updateItemPrice(CartEntityAddEvent $event) {
+//   	$response = $event->getResponse();
+  	drupal_set_message('Pridani nove polozky do kosiku');
+//   	$unitPrice = new Price(233, 'CZK');
+//   	$event->getOrderItem()->setUnitPrice($unitPrice);
   }
   
   /**
    * {@inheritdoc}
    */
   static function getSubscribedEvents() {
-  	$events[Drupal\commerce_cart\Event::CART_ENTITY_ADD][] = ['updateItemPrice'];
+		//$events[Drupal\commerce_cart\Event\CartEvents::CART_ENTITY_ADD][] = ['updateItemPrice'];
+  	$events['commerce_cart.entity.add'][] = ['updateItemPrice'];
   	
     return $events;
   }
