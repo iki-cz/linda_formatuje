b89e7263c69e755a104e9c923a01ec4533a79052
iki <iki@iki-pc>
2017-08-26 16:16:28 +0200

    working price update

--- src/EventSubscriber/ItemPriceSubscriber.php
+++ src/EventSubscriber/ItemPriceSubscriber.php
@@ -13,7 +13,6 @@ use Drupal\linda_formatuje\PriceCalculator;
 class ItemPriceSubscriber implements EventSubscriberInterface {
 
   public function updateItemPrice(CartEntityAddEvent $event) {
-//   	$response = $event->getResponse();
 		$orderItem = $event->getOrderItem();
 		
 		$pocetStranek = $orderItem->field_pocet_stranek->getString();
@@ -22,34 +21,10 @@ class ItemPriceSubscriber implements EventSubscriberInterface {
 		$priceCalculator = new PriceCalculator();
 		$price = (string) $priceCalculator ->calculate($pocetStranek, $horiTo);
 		$unitPrice = new Price($price, 'CZK');
-// 		$unitPrice = $orderItem->getUnitPrice();
-// 		$unitPrice->add($price);
-// 		$unitPrice->
-		$orderItem->setUnitPrice($unitPrice, true);
-		//$orderItem->save();
-// 		var_dump($orderItem->getAdjustments());
-// 		drupal_set_message(sprintf('pocet stranek %1$s', 
-// 				$orderItem->getData('field_pocet_stranek')));
-  	//drupal_set_message('Pridani nove polozky do kosiku');
-//   	$unitPrice = new Price(233, 'CZK');
-//   	$event->getOrderItem()->setUnitPrice($unitPrice, true); //true je overridden_unit_price
+		$orderItem->setUnitPrice($unitPrice, true); //true = overriden
+		$orderItem->save();
 
 		//http://docs.drupalcommerce.org/v2/recipes/orders.html
-// 		$unitPrice = new \Drupal\commerce_price\Price('9.11', 'CZK');
-// 		$orderItem->setUnitPrice($unitPrice, true); //true je overridden_unit_price
-// 		$orderItem->save();
-
-// 		$orderItem->getData($key);
-// 		$orderItem->get('data');
-// 		kint_require();
-// 		drupal_set_message(Kint::dump($orderItem));
-// 			ksm($orderItem);
-// 			var_dump(array());
-// 			var_dump($orderItem);die;
-		//$orderItem->setData($key, $value)
-// 		drupal_set_message(sprintf('Pocet stranek %1$s', 
-// 				$orderItem->field_pocet_stranek->getValue()));
-// 		drupal_set_message('Pridana polozka');
   }
   
   /**
