df728d9e83eafa5ce5665e55ee5a68e1f52d8eaa
iki <iki@iki-pc>
2017-08-27 06:56:22 +0200

    better price calculation

--- src/PriceCalculator.php
+++ src/PriceCalculator.php
@@ -4,10 +4,22 @@ namespace Drupal\linda_formatuje;
 class PriceCalculator{
 	const PAGE_PRICE = 19.9;
 	const EXPRESS_PRICE = 399;
-	
+	const BASE_PRICE = 999;
+	const BASE_PAGES_COUNT = 50;
 	
 	public function calculate($pocetStranek, $horiTo){
-		$price = intval($pocetStranek) * self::PAGE_PRICE + intval($horiTo) * self::EXPRESS_PRICE;
+		$price = self::BASE_PRICE;
+		
+		//pages 50+
+		if($pocetStranek > self::BASE_PAGES_COUNT){
+			$price += (intval($pocetStranek) - self::BASE_PAGES_COUNT) * self::PAGE_PRICE;
+		}
+
+		//express
+		$price += intval($horiTo) * self::EXPRESS_PRICE;
+		
+		$price = round($price);
+		
 		return $price;
 	}
 }
